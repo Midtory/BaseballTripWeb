7b4649791c32c8993390c3e663d005b1ace825f1
황석주 <asa4163dev@gmail.com>
2024-11-01 17:03:40 +0900

    Feat: Add PlanController Mapping
    Create Mapping likelist/save/share/list

--- baseball-trip-web-project/src/main/java/com/baseballtrip/plan/controller/PlanController.java
+++ baseball-trip-web-project/src/main/java/com/baseballtrip/plan/controller/PlanController.java
@@ -1,6 +1,10 @@
 package com.baseballtrip.plan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,27 @@ public class PlanController {
 		this.planService = planService;
 	}
 	
+	@PostMapping("/likelist")
+	public ResponseEntity<?> getLikePlanList() {
+		
+		return ResponseEntity.ok().build();
+	}
+	
+	@GetMapping("/save")
+	public ResponseEntity<?> savePlan() {
+		
+		return ResponseEntity.created(null).build();
+	}
+	
+	@PatchMapping("/share")
+	public ResponseEntity<?> sharePlan() {
+		
+		return ResponseEntity.ok(null);
+	}
+	
+	@GetMapping("/list")
+	public ResponseEntity<?> getPlanList() {
+		
+		return ResponseEntity.ok(null);
+	}
 }
