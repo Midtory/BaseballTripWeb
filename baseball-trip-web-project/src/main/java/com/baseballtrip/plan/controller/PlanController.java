f6ecaa7a629a08899755080bcd1363a48836b85c
황석주 <asa4163dev@gmail.com>
2024-11-01 17:29:43 +0900

    Feat: Modify PlanController
    list 세분화
    load 추가

--- baseball-trip-web-project/src/main/java/com/baseballtrip/plan/controller/PlanController.java
+++ baseball-trip-web-project/src/main/java/com/baseballtrip/plan/controller/PlanController.java
@@ -1,5 +1,7 @@
 package com.baseballtrip.plan.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baseballtrip.plan.dto.ResponseAllPlanDto;
+import com.baseballtrip.plan.dto.ResponseLikePlanDto;
+import com.baseballtrip.plan.dto.ResponsePlanDetailDto;
+import com.baseballtrip.plan.dto.ResponsePlanDto;
+import com.baseballtrip.plan.dto.ResponseRecommendPlanDto;
 import com.baseballtrip.plan.service.PlanService;
 
 @RestController
@@ -21,16 +28,42 @@ public class PlanController {
 		this.planService = planService;
 	}
 	
-	@PostMapping("/likelist")
-	public ResponseEntity<?> getLikePlanList() {
+	@GetMapping("/list/all")
+	public ResponseEntity<?> getAllPlanList() {
 		
-		return ResponseEntity.ok().build();
+		List<ResponseAllPlanDto> allPlanList = null;
+		
+		return ResponseEntity.ok(allPlanList);
 	}
 	
-	@GetMapping("/save")
+	@GetMapping("/list/like")
+	public ResponseEntity<?> getLikePlanList() {
+		
+		List<ResponseLikePlanDto> likePlanList = null;
+		
+		return ResponseEntity.ok(likePlanList);
+	}
+	
+	@GetMapping("/list/recommend")
+	public ResponseEntity<?> getRecommendPlanList() {
+		
+		List<ResponseRecommendPlanDto> recommendPlanList = null;
+		
+		return ResponseEntity.ok(recommendPlanList);
+	}
+	
+	@PostMapping("/save")
 	public ResponseEntity<?> savePlan() {
 		
 		return ResponseEntity.created(null).build();
+	}
+	
+	@GetMapping("/load")
+	public ResponseEntity<?> loadPlan() {
+		
+		ResponsePlanDetailDto responsePlanDetailDto = null;
+		
+		return ResponseEntity.ok(responsePlanDetailDto);
 	}
 	
 	@PatchMapping("/share")
